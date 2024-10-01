07308c91fef17a16c6332f72a93e2ea6dcef923f
abeeha7 <97190062+abeeha7@users.noreply.github.com>
2024-10-01 22:01:17 +0500

    main page images

--- my-react-app/src/pages/MainPage/Mainpage.jsx
+++ my-react-app/src/pages/MainPage/Mainpage.jsx
@@ -1,14 +1,19 @@
 import CategoryCard from "../../components/CategoryCards";
 import Animal from "../../assets/CategoryIcons/Animals.png";
+import Alphabets from "../../assets/CategoryIcons/alphabets.png"
+import Fruits from "../../assets/CategoryIcons/fruits.png";
+import Numbers from "../../assets/CategoryIcons/numbers.png"
+import Veggies from "../../assets/CategoryIcons/veggies.png"
+import Colors from "../../assets/CategoryIcons/colors.png"
 import "./Mainpage.css"
 const categories = [
-  { name: 'Alphabets', image: Animal },
+  { name: 'Alphabets', image: Alphabets },
   // Add other categories similarly
-  { name: 'Numbers', image: Animal },
-  { name: 'Fruits', image: Animal },
+  { name: 'Numbers', image: Numbers },
+  { name: 'Fruits', image: Fruits },
   { name: 'Animals', image: Animal },
-  { name: 'Veggies', image: Animal },
-  { name: 'Colors', image: Animal},
+  { name: 'Vegetables', image: Veggies },
+  { name: 'Colors', image: Colors},
 ];
 
 const Mainpage = () => {
